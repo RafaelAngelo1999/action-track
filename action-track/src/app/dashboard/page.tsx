63d1d86162085d09b25dcf7abfdcdb2b5c8b3cbd
rafael angelo <rafaelangelowow@gmail.com>
2024-07-24 00:16:34 -0300

    feat: :sparkles: ajustando layout default e dashboard

--- action-track/src/app/dashboard/page.tsx
+++ action-track/src/app/dashboard/page.tsx
@@ -4,8 +4,19 @@ import { Grid } from "@mui/material";
 export default function Dashboard() {
   return (
     <Grid>
-      <Grid item>
-        <ActionCard />
+      <Grid container>
+        <Grid item xs={6} sm={4} md={3}>
+          <ActionCard />
+        </Grid>
+        <Grid item xs={6} sm={4} md={3}>
+          <ActionCard />
+        </Grid>
+        <Grid item xs={6} sm={4} md={3}>
+          <ActionCard />
+        </Grid>
+        <Grid item xs={6} sm={4} md={3}>
+          <ActionCard />
+        </Grid>
       </Grid>
     </Grid>
   );
